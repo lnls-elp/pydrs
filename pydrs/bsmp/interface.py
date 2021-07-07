46fedbf03f9eec9aa7bb8bd3360f7c2d0a5ec406
carneirofc <claudiofcarneiro@hotmail.com>
2021-07-07 13:21:54 -0300

    bsmp: interface update

--- pydrs/bsmp/interface.py
+++ pydrs/bsmp/interface.py
@@ -1,3 +1,4 @@
+from time import time
 import typing
 import serial
 import serial.serialutil
@@ -13,34 +14,38 @@ class TCPInterface(_IOInterface):
 class SerialInterface(_IOInterface):
     def __init__(self, path: str, baudrate: int):
         super().__init__()
-        self._port = path
-        self._baudrate = baudrate
+        self._port: str = path
+        self._baudrate: int = baudrate
         self._serial: typing.Optional[serial.serialutil.SerialBase] = None
-        self._encoding = "utf-8"
+        self._encoding: str = "utf-8"
 
         self.open()
 
     def open(self) -> None:
         """Open the serial connection"""
+        if not self._serial:
+            self._serial = serial.Serial(port=self._port, baudrate=self._baudrate)
 
     def close(self) -> None:
+        """Close the serial connection"""
         if self._serial:
             self._serial.close()
+            self._serial = None
 
     def UART_read(self) -> typing.List[str]:
         # @todo: Usar a especificação do bsmp para ler o número correto de bytes
-        raise NotImplementedError
+        _response = self._serial.read_all()
+        _decoded = _response.decode(self._encoding)
+        return [s for s in _decoded]
 
     def UART_write(
         self, stream: typing.List[str], timeout: float
     ) -> typing.Optional[typing.Any]:
-        raise NotImplementedError
+        self._serial.write(stream)
 
     def UART_request(
         self, stream: typing.List[str], timeout: float
     ) -> typing.Optional[typing.List[str]]:
-        # @todo: Usar a especificação do bsmp para ler o número correto de bytes
-        self._serial.write(stream)
-        _response = self._serial.read_all()
-        _decoded = _response.decode(self._encoding)
-        return [s for s in _decoded]
+        """Read and Write OP"""
+        self.UART_write(stream, timeout=timeout)
+        return self.UART_read()
