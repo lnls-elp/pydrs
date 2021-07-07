20e72fa13a4d1b8146e1e20f00941f8570a03f59
carneirofc <claudiofcarneiro@hotmail.com>
2021-07-07 13:22:14 -0300

    bsmp: command typehint

--- pydrs/bsmp/command.py
+++ pydrs/bsmp/command.py
@@ -11,22 +11,24 @@ class PyDrsBasePS(_PSBSMP):
     Caso esse objeto seja nulo, diversas funções deverão ser implementadas novamente.
     ver from siriuspy.pwrsupply.bsmp.entities import EntitiesPS"""
 
-    _timeout_read_variable = 100  # [ms]
-    _timeout_execute_function = 100  # [ms]
-    _timeout_remove_vars_groups = 100  # [ms]
-    _timeout_create_vars_groups = 100  # [ms]
-    _timeout_read_group_of_variables = 100  # [ms]
-    _timeout_request_curve_block = 100  # [ms]
-    _timeout_curve_block = 100  # [ms]
-    _timeout_query_list_of_group_of_variables = 100  # [ms]
-    _timeout_query_group_of_variables = 100  # [ms]
+    _timeout_read_variable: float = 100  # [ms]
+    _timeout_execute_function: float = 100  # [ms]
+    _timeout_remove_vars_groups: float = 100  # [ms]
+    _timeout_create_vars_groups: float = 100  # [ms]
+    _timeout_read_group_of_variables: float = 100  # [ms]
+    _timeout_request_curve_block: float = 100  # [ms]
+    _timeout_curve_block: float = 100  # [ms]
+    _timeout_query_list_of_group_of_variables: float = 100  # [ms]
+    _timeout_query_group_of_variables: float = 100  # [ms]
 
     _sleep_turn_onoff = 0.050  # [s]
     _sleep_reset_udc = 1.000  # [s]
     _sleep_disable_scope = 0.5  # [s]
     _sleep_select_op_mode = 0.030  # [s]
 
-    def __init__(self, iointerface: _IOInterface, slave_address: int, entities: _EntitiesPS):
+    def __init__(
+        self, iointerface: _IOInterface, slave_address: int, entities: _EntitiesPS
+    ):
         super().__init__(iointerface, slave_address, entities)
         self._entities: _EntitiesPS
 
