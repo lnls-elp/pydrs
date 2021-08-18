0410f5d44bd5eee62d7b20c1d0118115cd00f94c
MarceloGMartins <marcelomartinsmgm@yahoo.com.br>
2021-08-18 00:38:03 -0300

    leagacy code removal

--- pydrs/bsmp/__init__.py
+++ pydrs/bsmp/__init__.py
@@ -2,57 +2,71 @@ import typing
 import serial
 import serial.serialutil
 
-
-from siriuspy.bsmp.serial import (
-    IOInterface as _IOInterface,
-)
-
-from siriuspy.bsmp.entities import Entities as _Entities
 from siriuspy.bsmp.commands import BSMP as _BSMP
+from siriuspy.bsmp.entities import Entities as _Entities
+from siriuspy.bsmp.serial import IOInterface as _IOInterface
 
 from siriuspy.pwrsupply.bsmp.entities import EntitiesPS as _EntitiesPS
+from siriuspy.pwrsupply.bsmp.constants import (
+    ConstPSBSMP as _ConstPSBSMP,
+    ConstFBP as _ConstFBP
+)
 
 
 class SerialInterface(_IOInterface):
     def __init__(self, path: str, baudrate: int):
+        # @notas: Responsável pela comunicação direta c/ o hardware.
+        # ": float"
+        #  -> typing.Optional[typing.Any]:
         super().__init__()
         self._path = path
         self._baudrate = baudrate
         self._serial: typing.Optional[serial.serialutil.SerialBase] = None
+        self._timeout = 1
+        self._encoding = 'utf-8'
 
     def open(self) -> None:
+        # 1 x Criar objeto serial
+        # 2 x Passar configurações ao objeto
+        # 3 x Conectar
+
         if not self._serial or self._serial.closed():
             # Open serial
-            pass
+            self._serial = serial.Serial(
+                port=self._path, baudrate=self._baudrate, timeout=self._timeout
+            )
 
     def close(self) -> None:
-        raise NotImplementedError
+        # Caso exista e esteja aberta, feche
+        if self._serial and not self._serial.closed():
+            # Close serial
+            self._serial.close()
+            self._serial = None
 
     def UART_read(self) -> typing.List[str]:
+        # Ler todos os bytes da serial, depois podemos usar 
+        # características do protocolo para ler apenas o número desejado
+        # Converter bytes -> str usando decode
+        # Converter str -> List[str]
+        #   b"ssssssss".decode('utf-8')
+        #    "ssssssss".encode('utf-8')
+        if not self._serial or self._serial.closed():
+            raise RuntimeError('Unable to open serial port')
+        port_readings = self._serial.read_all()
+        port_readings = port_readings.decode(self._encoding)
+        return [s for s in port_readings]
+        
+                    
+    def UART_write(self, stream:typing.List[str], timeout: float) -> typing.Optional[typing.Any]:
+        # Escrita
         raise NotImplementedError
 
-    def UART_write(self, stream, timeout: float) -> typing.Optional[typing.Any]:
-        raise NotImplementedError
-
-    def UART_request(self, stream, timeout: float) -> typing.Optional[typing.List[str]]:
-        raise NotImplementedError
-
-
-class PyDrsBasePS(_BSMP):
-    """Essa classe espera receber como parâmetro um objeto do tipo siriuspy.bsmp.Entities,
-    que possui a relação entre endereço e tipo de entidade bsmp que o dispositivo
-    que desejamos controlar possui. Caso esse objeto seja nulo, diversas funções
-    deverão ser implementadas novamente.
-    ver from siriuspy.pwrsupply.bsmp.entities import EntitiesPS"""
-
-    def __init__(self, pru: _IOInterface, slave_address: int, entities: _Entities):
-        super().__init__(pru, slave_address, entities)
-
-
-if __name__ == "__main__":
-    ser = SerialInterface(path="/serial.....")
-    entities = _EntitiesPS()
-
-    ps = PyDrsBasePS(pru=ser, entities=entities, slave_address=0)
-    res = ps.read_variable(var_id=0, timeout=500)
-    print(res)
+    def UART_request(
+        self, stream: typing.List[str], timeout: float
+    ) -> typing.Optional[typing.List[str]]:
+        # Escrita seguida por uma leitura
+        # 1 - Realizar escrita
+        self.UART_write(....)
+        # 2 - Realizar leitura
+        return self.UART_read(....)
+        
--- pydrs/constants/common_list.py
+++ pydrs/constants/common_list.py
@@ -5,153 +5,7 @@
 ======================================================================
 """ """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" """""" ""
 
-ListVar = [
-    "iLoad1",
-    "iLoad2",
-    "iMod1",
-    "iMod2",
-    "iMod3",
-    "iMod4",
-    "vLoad",
-    "vDCMod1",
-    "vDCMod2",
-    "vDCMod3",
-    "vDCMod4",
-    "vOutMod1",
-    "vOutMod2",
-    "vOutMod3",
-    "vOutMod4",
-    "temp1",
-    "temp2",
-    "temp3",
-    "temp4",
-    "ps_OnOff",
-    "ps_OpMode",
-    "ps_Remote",
-    "ps_OpenLoop",
-    "ps_SoftInterlocks",
-    "ps_HardInterlocks",
-    "iRef",
-    "wfmRef_Gain",
-    "wfmRef_Offset",
-    "sigGen_Enable",
-    "sigGen_Type",
-    "sigGen_Ncycles",
-    "sigGenPhaseStart",
-    "sigGen_PhaseEnd",
-    "sigGen_Freq",
-    "sigGen_Amplitude",
-    "sigGen_Offset",
-    "sigGen_Aux",
-    "dp_ID",
-    "dp_Class",
-    "dp_Coeffs",
-    "ps_Model",
-    "wfmRef_PtrBufferStart",
-    "wfmRef_PtrBufferEnd",
-    "wfmRef_PtrBufferK",
-    "wfmRef_SyncMode",
-]
-
-ListCurv = [
-    "wfmRef_Curve",
-    "sigGen_SweepAmp",
-    "samplesBuffer",
-    "fullwfmRef_Curve",
-    "wfmRef_Blocks",
-    "samplesBuffer_blocks",
-]
-
-ListFunc = [
-    "TurnOn",
-    "TurnOff",
-    "OpenLoop",
-    "ClosedLoop",
-    "OpMode",
-    "RemoteInterface",
-    "SetISlowRef",
-    "ConfigWfmRef",
-    "ConfigSigGen",
-    "EnableSigGen",
-    "DisableSigGen",
-    "ConfigDPModule",
-    "WfmRefUpdate",
-    "ResetInterlocks",
-    "ConfigPSModel",
-    "ConfigHRADC",
-    "ConfigHRADCOpMode",
-    "EnableHRADCSampling",
-    "DisableHRADCSampling",
-    "ResetWfmRef",
-    "SetRSAddress",
-    "EnableSamplesBuffer",
-    "DisableSamplesBuffer",
-    "SetISlowRefx4",
-    "SelectHRADCBoard",
-    "SelectTestSource",
-    "ResetHRADCBoards",
-    "Config_nHRADC",
-    "ReadHRADC_UFM",
-    "WriteHRADC_UFM",
-    "EraseHRADC_UFM",
-    "ReadHRADC_BoardData",
-]
-
-ListTestFunc = [
-    "UdcIoExpanderTest",
-    "UdcLedTest",
-    "UdcBuzzerTest",
-    "UdcEepromTest",
-    "UdcFlashTest",
-    "UdcRamTest",
-    "UdcRtcTest",
-    "UdcSensorTempTest",
-    "UdcIsoPlaneTest",
-    "UdcAdcTest",
-    "UdcUartTest",
-    "UdcLoopBackTest",
-    "UdcComTest",
-    "UdcI2cIsoTest",
-]
-
-ListHRADCInputType = [
-    "Vin_bipolar",
-    "Vin_unipolar_p",
-    "Vin_unipolar_n",
-    "Iin_bipolar",
-    "Iin_unipolar_p",
-    "Iin_unipolar_n",
-    "Vref_bipolar_p",
-    "Vref_bipolar_n",
-    "GND",
-    "Vref_unipolar_p",
-    "Vref_unipolar_n",
-    "GND_unipolar",
-    "Temp",
-    "Reserved0",
-    "Reserved1",
-    "Reserved2",
-]
-
-ListPSModels = [
-    "FBP_100kHz",
-    "FBP_Parallel_100kHz",
-    "FAC_ACDC_10kHz",
-    "FAC_DCDC_20kHz",
-    "FAC_Full_ACDC_10kHz",
-    "FAC_Full_DCDC_20kHz",
-    "FAP_ACDC",
-    "FAP_DCDC_20kHz",
-    "TEST_HRPWM",
-    "TEST_HRADC",
-    "JIGA_HRADC",
-    "FAP_DCDC_15kHz_225A",
-    "FBPx4_100kHz",
-    "FAP_6U_DCDC_20kHz",
-    "JIGA_BASTIDOR",
-]
-
-ListPSModels_v2_1 = [
+list_ps_models = [
     "Empty",
     "FBP",
     "FBP_DCLink",
@@ -186,7 +40,7 @@ ListPSModels_v2_1 = [
     "Uninitialized",
 ]
 
-ListVar_v2_1 = [
+list_common_vars = [
     "ps_status",
     "ps_setpoint",
     "ps_reference",
@@ -210,10 +64,10 @@ ListVar_v2_1 = [
     "p_wfmref_idx",
 ]
 
-# ListCurv_v2_1 = ['wfmref','buf_samples_ctom','buf_samples_mtoc']
-ListCurv_v2_1 = ["wfmref_data_0", "wfmref_data_1", "buf_samples_ctom"]
+# list_curv = ['wfmref','buf_samples_ctom','buf_samples_mtoc']
+list_curv = ["wfmref_data_0", "wfmref_data_1", "buf_samples_ctom"]
 
-ListFunc_v2_1 = [
+list_func = [
     "turn_on",
     "turn_off",
     "open_loop",
@@ -260,7 +114,7 @@ ListFunc_v2_1 = [
     "reset_udc",
 ]
 
-ListOpMode_v2_1 = [
+list_op_mode = [
     "Off",
     "Interlock",
     "Initializing",
@@ -272,7 +126,7 @@ ListOpMode_v2_1 = [
     "FastRef",
 ]
 
-ListSigGenTypes_v2_1 = [
+list_sig_gen_types = [
     "Sine",
     "DampedSine",
     "Trapezoidal",
@@ -280,7 +134,7 @@ ListSigGenTypes_v2_1 = [
     "Square",
 ]
 
-ListParameters = [
+list_parameters = [
     "PS_Name",
     "PS_Model",
     "Num_PS_Modules",
@@ -345,17 +199,4 @@ ListParameters = [
     "",
     "Password",
     "Enable_Onboard_EEPROM",
-]
-
-ListBCBFunc = [
-    "ClearPof",
-    "SetPof",
-    "ReadPof",
-    "EnableBuzzer",
-    "DisableBuzzer",
-    "SendUartData",
-    "GetUartData",
-    "SendCanData",
-    "GetCanData",
-    "GetI2cData",
 ]
