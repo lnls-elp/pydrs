22f55ba3e2cb7c4274a15cabdea1224c3543f32f
MarceloGMartins <marcelomartinsmgm@yahoo.com.br>
2021-08-26 16:33:01 -0300

    out of range interlock fixed

--- src/pydrs/pydrs.py
+++ src/pydrs/pydrs.py
@@ -1556,14 +1556,29 @@ class SerialDRS(object):
             self.get_wfmref_vars(1)
             self.get_scope_vars()
 
+    def _interlock_unknown_assignment(self, active_interlocks, index):
+        active_interlocks.append("bit {}: Reserved".format(index))
+
+    def _interlock_name_assigned(self, active_interlocks, index, list_interlocks):
+        active_interlocks.append(
+            "bit {}: {}".format(index, list_interlocks[index])
+        )
+
     def decode_interlocks(self, reg_interlocks, list_interlocks):
         active_interlocks = []
+        for index in range(32):
+            if reg_interlocks & (1 << index):
+                if index < len(list_interlocks):
+                    _interlock_name_assigned(
+                        active_interlocks, index, list_interlocks
+                    )
+                else:
+                    _interlock_unknown_assignment(
+                        active_interlocks, index
+                    )
 
-        for i in range(32):
-            if reg_interlocks & (1 << i):
-                active_interlocks.append(list_interlocks[i])
-                print("\t" + list_interlocks[i])
-        return active_interlocks
+        for index, interlock in enumerate(active_interlocks):
+            print(interlock)
 
     def read_vars_fbp(self, n=1, dt=0.5):
 
